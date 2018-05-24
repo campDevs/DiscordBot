c3e59612ab1c2c408677857dc32c617e5d8e99bf
pahosler <pahosler@gmail.com>
2018-05-24 00:59:52 -0500

    update horoscope.js

--- src/commands/fun/horoscope.js
+++ src/commands/fun/horoscope.js
@@ -1,29 +1,6 @@
-/*
-Copyright (c) 2018 Paul Hosler
-
-The MIT License (MIT)
-copyright © 2015 <copyright holders>
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
-documentation files (the “Software”), to deal in the Software without restriction, including without 
-limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
-the Software, and to permit persons to whom the Software is furnished to do so, subject to the following
-conditions:
-The above copyright notice and this permission notice shall be included in all copies or substantial portions 
-of the Software.
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
-TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT 
-SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
-ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE 
-OR OTHER DEALINGS IN THE SOFTWARE.
-
-https://opensource.org/licenses/MIT
-*/
-
-
+// @Author Paul Hosler
 // required modules for this feature
-const {
-  Command
-} = require('discord.js-commando');
+const {Command} = require('discord.js-commando');
 // const fetch = require('node-fetch');
 const logger = require('../../utils/logging.js');
 const getHoroscope = require('../../utils/getHoroscope.js');
@@ -58,28 +35,7 @@ module.exports = class Horoscope extends Command {
     });
   }
 
-  async run(msg, {
-    sign
-  }) {
-    // fetch(`https://confused-closet.glitch.me/api?sign=${sign}`)
-    //   .then((res) => res.text())
-    //   .then((body) => body)
-    //   .then(JSON.parse)
-    //   .then((message) => {
-    //     if (message.error !== undefined) {
-    //       return msg.say(`I only know these Zodiac symbols: ${message.error}`);
-    //     }
-    //     return msg.say(
-    //       `\n${message.sign.name} ${message.sign.start} thru ${
-    //         message.sign.end
-    //       }\nHoroscope for ${readableDate(message.date)}\n${
-    //         message.description
-    //       }`
-    //     );
-    //   })
-    //   .catch((error) => {
-    //     logger.error(error);
-    //   });
+  async run(msg, {sign}) {
     let outlook = await getHoroscope(sign)
       .then((message) => {
         if (message.error !== undefined) {
