30da510cca3078af4dcfea97ec4e2a9a36dfb581
Paul Hosler <pahosler@gmail.com>
2018-05-24 14:20:07 -0500

    update with requested changes

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,10 @@
-require('dotenv').config()
-const config = require('./config.json')
-const logger = require('./utils/logging.js')
-const { CommandoClient } = require('discord.js-commando')
-const path = require('path')
+require("dotenv").config()
+const config = require("./config.json")
+const logger = require("./utils/logging.js")
+const {
+  CommandoClient
+} = require("discord.js-commando")
+const path = require("path")
 
 const client = new CommandoClient({
   commandPrefix: config.prefix,
@@ -13,42 +15,41 @@ const client = new CommandoClient({
 client.registry
   .registerDefaultTypes()
   .registerGroups([
-    ['music', 'Music related commands'],
-    ['code', 'Code related commands'],
-    ['assistants', 'Commands to assist the bot overlord(s)'],
-    ['humor', "Bot's humor processing unit"],
-    ['scrapers', 'Scrape for data from remote sources'],
-    ['fetch', 'Fetch data from websites'],
-    ['fun', 'Games our bot likes to play'],
-  ])
-  .registerDefaultGroups()
-  .registerDefaultCommands({ eval: false })
-  .registerCommandsIn(path.join(__dirname, 'commands'))
+      ["music", "Music related commands"],
+      ["code", "Code related commands"],
+      ["assistants", "Commands to assist the bot overlord(s)"],
+      ["humor", "Bot"
+        s humor processing unit "], ["scrapers", "Scrape for data from remote sources"],
+        ["fetch", "Fetch data from websites"],
+        ["fun", "Games our bot likes to play"],
+      ])
+    .registerDefaultGroups()
+    .registerDefaultCommands({
+      eval: false
+    })
+    .registerCommandsIn(path.join(__dirname, "commands"))
 
-client.on('error', error => logger.error(error))
-client.on('warn', warning => logger.warn(warning))
-client.on('debug', dbgMsg => logger.debug(dbgMsg))
-client.on('ready', () => {
-  logger.info(`
+    client.on("error", error => logger.error(error)) client.on("warn", warning => logger.warn(warning)) client.on("debug", dbgMsg => logger.debug(dbgMsg)) client.on("ready", () => {
+      logger.info(`
   Horrorhaku Bot
      T
    .-"-.
   |  ___|
   | (.\\/.)
-  |  ,,,' 
-  | '###
-  '----'  
+  |  ,,," 
+  | "###
+  "----"  
   Connected as: ${client.user.tag}!`)
-  client.user.setActivity('Barbie Adventures')
-})
+      client.user.setActivity("Barbie Adventures")
+    })
 
-// React randomly to other bots
-client.on('message', async (msg) => {
-  if (msg.author.bot) {
-    if (msg.author.id !== client.user.id && Math.random() <= 0.1) {
-      msg.react('🤖')
-    }
-  }
-})
+    // React randomly to other bots
+    client.on("message", async (msg) => {
+      if (msg.author.bot) {
+        if (msg.author.id !== client.user.id && Math.random() <= 0.1) {
+          msg.react("🤖")
+        }
+      }
+    })
 
-client.login(process.env.TOKEN)
+    client.login(process.env.TOKEN)
---
+++ src/utils/getHoroscope.js
@@ -0,0 +1,142 @@
+/*
+Copyright (c) 2018 pahosler
+
+The MIT License (MIT)
+copyright © 2015 <copyright holders>
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the “Software”), to deal in the Software without restriction, including without 
+limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
+the Software, and to permit persons to whom the Software is furnished to do so, subject to the following
+conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+of the Software.
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT 
+SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE 
+OR OTHER DEALINGS IN THE SOFTWARE.
+
+https://opensource.org/licenses/MIT
+horoscope data scraped from http://astrology.kudosmedia.net
+**/
+
+const fetch = require('node-fetch');
+const fs = require('fs');
+const path = require('path');
+
+const SIGNS = [
+  'capricorn',
+  'aquarius',
+  'pisces',
+  'aries',
+  'taurus',
+  'gemini',
+  'cancer',
+  'leo',
+  'virgo',
+  'libra',
+  'scorpio',
+  'sagittarius'
+];
+
+// http://astrology.kudosmedia.net requires a header to access the pages
+const Options = {
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML,' +
+      ' like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25'
+  }
+};
+
+// Useful functions that return a value 
+
+// return an array of URLs containing each horoscope
+const urlArray = () => SIGNS.map(
+  sign => `http://astrology.kudosmedia.net/index.php/m/${sign}?day=today`);
+
+// return todays date as ex. 2018-5-8
+const today = () => new Date(Date.now()).toISOString().split('T')[0];
+
+// check for valid sign request
+const isSign = (sign) => SIGNS.includes(sign.toLowerCase());
+
+// Promise block - read and return the horoscope.json file
+const getZodiac = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path.join(__dirname, 'horoscope.json'), 'utf8', (err, content) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(content);
+      }
+    });
+  });
+}
+
+// async function to write stringified JSON to horoscope.json
+function writeJSON(data) {
+  fs.writeFileSync(path.join(__dirname, 'horoscope.json'), JSON.stringify(data));
+  return
+}
+
+// Promise block - get horoscope data asyncronously from remote source
+const get = url => {
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+        Options
+      })
+      .then(res => res.text())
+      .then(body => {
+        resolve(body.match(/horoscope\s=?\s(.*)?;/)[1]);
+      })
+      .catch(error => {
+        reject(error);
+      })
+  })
+}
+
+// use the Promise to get data and parse JSON
+const getJSON = url => {
+  return get(url)
+    .then(JSON.parse)
+    .catch(err => {
+      console.error(`getJSON failed for: ${url} ${err}`)
+    })
+}
+
+const getHoroscope = () => Promise.all(urlArray().map(getJSON));
+
+const loadZodiac = () => Promise.resolve(getZodiac());
+
+const updateHoroscope = () => {
+  let content = {}
+  getHoroscope()
+    .then((horoscope, content = {}) => {
+      horoscope.forEach((outlook, index) => {
+        content[SIGNS[index]] = outlook;
+      });
+      return content
+    })
+    .then(content => {
+      writeJSON(content);
+      return
+    });
+}
+
+async function Horoscope(sign) {
+  if (!isSign(sign)) {
+    return {
+      error: `${SIGNS}`
+    }
+  }
+
+  return loadZodiac()
+    .then(JSON.parse)
+    .then(zodiac => {
+      if (today() !== zodiac[sign].date) {
+        updateHoroscope();
+      }
+      return zodiac[sign];
+    })
+}
+
+module.exports = Horoscope;
