31ee6f556cc193d1b9eefbfc67b031fa62e7e33b
Jody LeCompte <38302762+jodylecompte@users.noreply.github.com>
2018-05-24 19:32:10 -0500

    Resolves #38 (#39)
    * Resolves #38
    
    Correctly formatted function so entirety of command code is inside
    try catch
    
    * Use logging over console.log

--- src/commands/scrapers/PHPDocCommand.js
+++ src/commands/scrapers/PHPDocCommand.js
@@ -1,7 +1,7 @@
 const {Command} = require('discord.js-commando')
 const cheerio = require('cheerio')
 const fetch = require("node-fetch")
-
+const logger = require('../../utils/logging');
 
 module.exports = class PHPDocCommand extends Command {
   constructor(client) {
@@ -39,6 +39,8 @@ module.exports = class PHPDocCommand extends Command {
       const $ = cheerio.load(text)	
       pageTitle = $('title').html()
 
+      throw new Error('ahhh noo, say it aint so :\'(');
+
       if(/Closest matches:/.test(text)) {
         exactMatch = false
         const recommendations = $('#quickref_functions li a')
@@ -49,25 +51,25 @@ module.exports = class PHPDocCommand extends Command {
         }
 
         urlString = urls.join('\n') 
-      } 
-    } catch(error) {
-      console.error(error)
-    }
-
-    if(exactMatch) {
-      return msg.embed(
-        {
-          title: pageTitle,
-          description: `The official documentation for "${query}"`,
-          url: `https://php.net/${encodedQuery}`,
-          color: 0x1e701e,
-          image: {
-            url: 'http://php.net/images/logos/new-php-logo.png'
+      }
+      
+      if(exactMatch) {
+        return msg.embed(
+          {
+            title: pageTitle,
+            description: `The official documentation for "${query}"`,
+            url: `https://php.net/${encodedQuery}`,
+            color: 0x1e701e,
+            image: {
+              url: 'http://php.net/images/logos/new-php-logo.png'
+            }
           }
-        }
-      )	
+        )	
+      }
+  
+      return msg.say(`I did not find an exact match for "${query}". Did you mean one of these? \n${urlString}`)
+    } catch(error) {
+      logger.error(error)
     }
-
-    return msg.say(`I did not find an exact match for "${query}". Did you mean one of these? \n${urlString}`)
   }
 }
