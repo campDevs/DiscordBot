5e9577082c8d002a14d7d605b547232c823e26fb
Glen Westbrook <mgw@tutanota.com>
2018-06-30 20:58:47 -0500

    Add deafen and undeafen commands

---
+++ src/commands/admin/Undeafen.js
@@ -0,0 +1,36 @@
+const {Command} = require('discord.js-commando')
+
+module.exports = class UndeafenCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: "undeafen",
+      group: "admin",
+      memberName: "undeafen",
+      description: "Undeafen a member allowing them to hear voice channels",
+      examples: [
+        "undeafen @IAm12",
+        "undeafen @IAm12 Safe to listen"
+      ],
+      args: [
+        {
+          key: "member",
+          prompt: "Who do you want to undeafen?",
+          type: "member"
+        },
+        {
+          key: "reason",
+          prompt: "Why do you want to undeafen them?",
+          type: "string",
+          default: "'None provided'"
+        }
+      ],
+      clientPermissions: ["DEAFEN_MEMBERS"],
+      userPermissions: ["DEAFEN_MEMBERS"]
+    })
+  }
+
+  run(msg, {member, reason}) {
+    member.setDeaf(false, reason)
+    msg.reply(`${member} was undeafened.`)
+  }
+}
