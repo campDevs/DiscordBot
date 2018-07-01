9e0a72466f020a9ae310b7d22aa59e82aabf9861
Glen Westbrook <mgw@tutanota.com>
2018-06-30 20:59:17 -0500

    Add git mute and unmute commands

---
+++ src/commands/admin/Mute.js
@@ -0,0 +1,36 @@
+const {Command} = require('discord.js-commando')
+
+module.exports = class MuteCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: "mute",
+      group: "admin",
+      memberName: "mute",
+      description: "Set member muted on voice channels",
+      examples: [
+        "mute @MicSpammer",
+        "mute @MicSpammer Singing endlessly"
+      ],
+      args: [
+        {
+          key: "member",
+          prompt: "Who do you want to mute?",
+          type: "member"
+        },
+        {
+          key: "reason",
+          prompt: "Why do you want to mute them?",
+          type: "string",
+          default: "'None provided'"
+        }
+      ],
+      clientPermissions: ["MUTE_MEMBERS"],
+      userPermissions: ["MUTE_MEMBERS"]
+    })
+  }
+
+  run(msg, {member, reason}) {
+    member.setMute(true, reason)
+    msg.reply(`${member} was muted.`)
+  }
+}
