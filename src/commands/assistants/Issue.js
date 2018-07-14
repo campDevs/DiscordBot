af626f0fb8ec0ac61964fd2a87182b4a38131dbf
jokebookservice1 <joker314@users.noreply.github.com>
2018-07-14 19:26:29 +0100

    Rename class to match its purpose

--- src/commands/assistants/Issue.js
+++ src/commands/assistants/Issue.js
@@ -1,6 +1,6 @@
 const {Command} = require('discord.js-commando')
 
-module.exports = class LmgtfyCommand extends Command {
+module.exports = class IssueCommand extends Command {
   constructor(client) {
     super(client, {
       name: "issue",
