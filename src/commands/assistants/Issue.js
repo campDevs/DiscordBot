159a765293992a513232bf0d40471a4015a904b0
jokebookservice1 <jokebookyeye@gmail.com>
2018-07-30 19:23:25 +0300

    Add better examples; make link text clearer per request

--- src/commands/assistants/Issue.js
+++ src/commands/assistants/Issue.js
@@ -10,8 +10,8 @@ module.exports = class IssueCommand extends Command {
       description: "Link to report bugs and request features about the bot.",
       examples: [
         "issue",
-        "issue Add better memes",
-		"issue Add better memes // Let's add better meme support. For example, none of the memes are memes."],
+        "issue Title of my issue",
+		"issue Title of my issue // Description of my issue"],
       args: [
         {
           key: "content",
@@ -27,7 +27,7 @@ module.exports = class IssueCommand extends Command {
     const [title, body] = content.split("//").map(s => s.trim()).concat("", "")
 	return msg.channel.send({
 		embed: {
-			title: "File an issue (for bugs and feature requests about this bot) on GitHub",
+			title: "CLICK HERE to File an issue (for bugs and feature requests about this bot) on GitHub",
 			description: title ? "The content you specified will be prefilled" : "",
 			url: "https://github.com/campDevs/DiscordBot/issues/new/" + (title && `?title=${encodeURIComponent(title)}`) + (body && `&body=${encodeURIComponent(body)}`),
 			footer: {"text": "Making GitHub issues requires a free account."}
