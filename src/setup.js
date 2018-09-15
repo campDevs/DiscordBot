3b96d70ef0a04b71e59e2c9dcb1c557fd78903f3
jokebookservice1 <jokebookyeye@gmail.com>
2018-09-15 01:25:08 +0100

    feat(CLI): Add output to CLI
    Still buggy, but this allows the CLI to read input and to emit output to
    the user.

--- src/setup.js
+++ src/setup.js
@@ -5,9 +5,23 @@ const rl = readline.createInterface({
   output: process.stdout
 })
 
-const defaults = require('')_
+const defaults = require('./defaults.json')
 const config = {}
 
+/**
+ * An array of objects to document which fields are needed for
+ * configuration files. Each entry should have the following
+ * fields.
+ *
+ * - field: the name of the field in the config file
+ * - type: array/boolean/string - the expected type of value from the user
+ * - prompt: a string to display to the user to help them provide an answer
+ *
+ * In addition, the following fields may be optionally be provided.
+ *
+ * - onlyIf: a function which takes an input representing the configuration file so far,
+ *   and returns true only if the user should be prompted for this field.
+ */
 const CONFIG_FIELDS = [
   {
     field: 'owners',
@@ -31,3 +45,79 @@ const CONFIG_FIELDS = [
     prompt: 'Channel where admin actions will be broadcast to'
   }
 ]
+
+/**
+ * An object to document the strings that should be appended to
+ * prompts. The key should be the type (as stated in CONFIG_FIELDS).
+ *
+ * The default is the empty string. 
+ */
+const howToAnswer = {
+  array: '(space-seperated values)',
+  boolean: '(y/n)'
+}
+
+/**
+ * How to convert an input into the object that should be added
+ * to the configuration file. The default is the identity function. (s => s)
+ *
+ * They key should be the type (as stated in CONFIG_FIELDS)
+ */
+const mapTypes = {
+  array: s => s.split(' '),
+  boolean: s => s.toLowerCase().startsWith('y')
+}
+
+/**
+ * An object to check that an input from the user isn't malformed.
+ * If it is, the user will be re-prompted. "true" means that the
+ * input is NOT malformed, but is instead valid. The default is (() => true)
+ *
+ * The key should be the type (as stated in CONFIG_FIELDS)
+ */
+const validateTypes = {
+  boolean: s => ['y', 'yes', 'n', 'no'].includes(s.toLowerCase())
+}
+
+handleField(CONFIG_FIELDS.shift())
+
+function handleField(configElement) {
+  if(configElement.onlyIf && !configElement.onlyIf(config)) {
+    return
+  }
+
+  if(typeof configElement === 'undefined') {
+    return
+  }
+
+  const mapper = mapTypes[configElement.type] || (s => s)
+  const validator = validateTypes[configElement.type] || (() => true)
+  
+  const help = configElement.prompt
+  const helpForType = howToAnswer[configElement.type] || ''
+
+  const text = help + (': ' + helpForType + ' ' + helpForType).replace(/ +/, ' ')
+
+  recursiveQuestion(text, validator, answer => {
+    config[configElement.field] = mapper(answer)
+    handleField(CONFIG_FIELDS.shift())
+  })
+}
+
+/**
+ * Prompt the user for an input until the validation function passes.
+ * When it does, call the callback.
+ *
+ * @param {string} arg - The text to use to prompt the user
+ * @param {function} conditionFunction - The function to determine whether to stop prompting the user
+ * @param {function} cb - The callback to invoke when conditionFunction returns true
+ */
+function recursiveQuestion(arg, conditionFunction, cb) {
+  rl.question(arg, answer => {
+    if(conditionFunction()) {
+      cb(answer)
+    } else {
+      recursiveQuestion(...arguments)
+    }
+  })
+}
